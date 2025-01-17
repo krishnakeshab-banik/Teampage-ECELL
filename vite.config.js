4790255e91b2251a657c7f95b53fd6123976e12e
krishnakeshab-banik <krishna.keshab.banik@gmail,com>
2025-01-17 22:55:12 +0530

    Fix asset paths for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/ecell-team/",
-  plugins: [react()]
+  base: '/ecell-team/', // Match your repository name
+  plugins: [react()],
 });
+
